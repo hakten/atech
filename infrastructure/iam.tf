eb29407179d7552174ccd5023d68635e858b4077
Huseyin Akten <husakten@gmail.com>
2020-01-18 23:40:18 -0600

    tag added

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,10 +1,14 @@
 resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/"
+
+  tags = {
+    Project = "atech"
+  }
 }
 
 
 resource "aws_iam_group_policy_attachment" "developers-attach" {
-  group      = "${aws_iam_group.developers.name}"
+  group      = aws_iam_group.developers.name
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
