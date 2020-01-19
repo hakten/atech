447d3599c5f83582a9c2ce809baa12824d17df56
Huseyin Akten <husakten@gmail.com>
2020-01-18 21:05:45 -0600

    path corrected

--- infrastructure/environment-backend-buckets/environments-backend-buckets-module.tf
+++ infrastructure/environment-backend-buckets/environments-backend-buckets-module.tf
@@ -1,26 +1,26 @@
 module "atech-backend-dev" {
-  source = "./environments-backend-buckets"
+  source = "./environments-backend-buckets-module"
   
   region        = "us-east-1"
   bucket_name   = "atech-backend-dev"
 }
 
 module "atech-backend-qa" {
-  source = "./environments-backend-buckets"
+  source = "./environments-backend-buckets-module"
   
   region        = "us-east-2"
   bucket_name   = "atech-backend-qa"
 }
 
 module "atech-backend-stage" {
-  source = "./environments-backend-buckets"
+  source = "./environments-backend-buckets-module"
   
   region        = "us-west-1"
   bucket_name   = "atech-backend-stage"
 }
 
 module "atech-backend-prod" {
-  source = "./environments-backend-buckets"
+  source = "./environments-backend-buckets-module"
   
   region        = "us-west-2"
   bucket_name   = "atech-backend-prod"
