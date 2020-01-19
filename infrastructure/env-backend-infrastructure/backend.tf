692bdd760ddc5d08f4f2aa7f5a662f8dda0d517c
Huseyin Akten <husakten@gmail.com>
2020-01-18 23:23:28 -0600

    iam group added

--- infrastructure/env-backend-infrastructure/backend.tf
+++ infrastructure/env-backend-infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "atech-backend"
-    key    = "infrastructure.tfstate"
+    key    = "env-backend-infrastructure.tfstate"
     region = "eu-west-1"
     dynamodb_table = "atech"
   }
---
+++ infrastructure/iam.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/"
+}
+
+# data "aws_iam_group" "power" {
+#   group_name = ""
+# }
+
+# resource "aws_iam_group_policy_attachment" "test-attach" {
+#   group      = "${aws_iam_group.group.name}"
+#   policy_arn = "${aws_iam_policy.policy.arn}"
+# }
