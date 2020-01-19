5b636d324f9543fb2cd76dc1bdc8576f2a1ea2c9
Huseyin Akten <husakten@gmail.com>
2020-01-18 22:12:22 -0600

    dynamodb

--- sadf/environment-backend-dynamodb-module.tf
+++ sadf/environment-backend-dynamodb-module.tf
@@ -1,31 +1,31 @@
 module "atech-backend-dynamodb-dev" {
   source = "./environment-backend-dynamodb-module"
   
-  region        = "us-east-1"
-  name          = "atech-dev"
-  project       = "atech"
+  # region        = "us-east-1"
+  # name          = "atech-dev"
+  # project       = "atech"
 }
 
-module "atech-backend-dynamodb-qa" {
-  source = "./environment-backend-dynamodb-module"
+# module "atech-backend-dynamodb-qa" {
+#   source = "./environment-backend-dynamodb-module"
   
-  region        = "us-east-2"
-  name          = "atech-qa"
-  project       = "atech"
-}
+#   region        = "us-east-2"
+#   name          = "atech-qa"
+#   project       = "atech"
+# }
 
-module "atech-backend-dynamodb-stage" {
-  source = "./environment-backend-dynamodb-module"
+# module "atech-backend-dynamodb-stage" {
+#   source = "./environment-backend-dynamodb-module"
   
-  region        = "us-west-1"
-  name          = "atech-stage"
-  project       = "atech"
-}
+#   region        = "us-west-1"
+#   name          = "atech-stage"
+#   project       = "atech"
+# }
 
-module "atech-backend-dynamodb-prod" {
-  source = "./environment-backend-dynamodb-module"
+# module "atech-backend-dynamodb-prod" {
+#   source = "./environment-backend-dynamodb-module"
   
-  region        = "us-west-2"
-  name          = "atech-prod"
-  project       = "atech"
-}
+#   region        = "us-west-2"
+#   name          = "atech-prod"
+#   project       = "atech"
+# }
