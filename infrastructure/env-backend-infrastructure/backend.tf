968d033392f36db375ddaedb5aefab16f5bcac9a
Huseyin Akten <husakten@gmail.com>
2020-01-20 00:25:41 -0600

    name changed

--- infrastructure/env-backend-infrastructure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "atech-backend"
-    key    = "env-backend-infrastructure.tfstate"
-    region = "eu-west-1"
-    dynamodb_table = "atech-backend"
-  }
-}
