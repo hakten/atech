0a6ff383ba67ed263b25d5d009a960b9892381f6
Huseyin Akten <husakten@gmail.com>
2020-01-19 20:51:30 -0600

    modules created

--- modules/module-vpc.tf
+++ modules/module-vpc.tf
@@ -11,35 +11,6 @@ public_subnets  = "${var.public_subnets}"
 private_subnets = "${var.private_subnets}"
 }
 
-variable "region" {}
-
-variable "cidr" {}
-variable "project" {}
-variable "environment" {}
-
-variable "azs" {
-    type = "list"
-}
-variable "public_subnets" {
-    type = "list"
-}
-variable "private_subnets" {
-    type = "list"
-}
 
 
-output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
-}
 
-output "vpc_cidr" {
-  value = "${module.vpc.vpc_cidr}"
-}
-
-output "public_subnets" {
-  value = "${module.vpc.public_subnets}"
-}
-
-output "private_subnets" {
-  value = "${module.vpc.private_subnets}"
-}
