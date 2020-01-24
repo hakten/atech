ec1383cf04ac2b876d0f701bf2762392496b95cf
Huseyin Akten <husakten@gmail.com>
2020-01-24 15:56:23 -0600

    sdaf

--- architecture/iam-developers-group-and-policy.tf
+++ architecture/iam-developers-group-and-policy.tf
@@ -11,14 +11,14 @@ resource "aws_iam_group_policy_attachment" "group-attachment" {
 
 resource "aws_iam_user" "users" {
   count = "${length(var.iam_users)}"
-  name  = "${element(var.iam_users,count.index)}"
+  name  = var.iam_users[count.index]
 }
 
 
 resource "aws_iam_group_membership" "membership" {
   name = "group-membership"
   count = "${length(var.iam_users)}"
-  users = [element(aws_iam_user.users.*.name,count.index)]
+  users = [aws_iam_user.users.name[count.index]]
 
   group = "${aws_iam_group.group.name}"
 }
