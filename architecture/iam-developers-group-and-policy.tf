49fa289de208f0f7d0a7cc0c63e38cbb4f8bee36
Huseyin Akten <husakten@gmail.com>
2020-01-24 15:58:55 -0600

    asdfa

--- architecture/iam-developers-group-and-policy.tf
+++ architecture/iam-developers-group-and-policy.tf
@@ -18,7 +18,7 @@ resource "aws_iam_user" "users" {
 resource "aws_iam_group_membership" "membership" {
   name = "group-membership"
   count = "${length(var.iam_users)}"
-  users = aws_iam_user.users[count.index].name
+  users = [aws_iam_user.users[count.index].name]
 
   group = "${aws_iam_group.group.name}"
 }
