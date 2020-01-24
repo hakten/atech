a1d4429e356ab5f77768bbd01b1d01a3b3df6b1c
Huseyin Akten <husakten@gmail.com>
2020-01-24 00:45:41 -0600

    sdf

--- architecture/iam-developers-group-and-policy.tf
+++ architecture/iam-developers-group-and-policy.tf
@@ -9,17 +9,17 @@ resource "aws_iam_group_policy_attachment" "group-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/"${var.iam_group_policy_name}"
 }
 
-resource "aws_iam_user" "users" {
-  count = "${length(var.iam_users)}"
-  name  = "${element(var.iam_users,count.index)}"
-}
+# resource "aws_iam_user" "users" {
+#   count = "${length(var.iam_users)}"
+#   name  = "${element(var.iam_users,count.index)}"
+# }
 
 
-resource "aws_iam_group_membership" "membership" {
-  name = "group-membership"
-  count = "${length(var.iam_users)}"
-  users = "${element(aws_iam_users.users.*.name,count.index)}"
+# resource "aws_iam_group_membership" "membership" {
+#   name = "group-membership"
+#   count = "${length(var.iam_users)}"
+#   users = "${element(aws_iam_users.users.*.name,count.index)}"
 
-  group = "${aws_iam_group.group.name}"
-}
+#   group = "${aws_iam_group.group.name}"
+# }
 
