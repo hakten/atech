f7b30f1b3707fa2a04076f908d32714da017befd
Huseyin Akten <husakten@gmail.com>
2020-01-23 23:51:14 -0600

    key pair edited

--- architecture/instance-nagios.tf
+++ architecture/instance-nagios.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "nagios" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.bastion.key_name
+  key_name      = aws_key_pair.bastion_key.key_name
   subnet_id     = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.ssh.id]
   
