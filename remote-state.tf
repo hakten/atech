13f0f041f8411903a3c115b78ece48e04162b70c
Huseyin Akten <husakten@gmail.com>
2020-01-24 16:30:25 -0600

    sadfa

--- remote-state.tf
+++ remote-state.tf
@@ -5,6 +5,7 @@ data "terraform_remote_state" "main" {
     bucket = "t-s3-backend"
     key    = "baytech/arch/infrastructure.tfstate"
 }
+}
 
 output "full_list" { 
   value = data.terraform_remote_state.main.* 
