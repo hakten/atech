5372feb7b9cdce7fa849a245560eec4977875e7a
Huseyin Akten <husakten@gmail.com>
2020-01-23 23:43:24 -0600

    sec group edited

--- architecture/security-groups.tf
+++ architecture/security-groups.tf
@@ -43,21 +43,21 @@ ingress {
   } 
 }
 
-resource "aws_security_group" "nagios_client" { 
-  name        = "nagios_client" 
-  description = "Allow db inbound traffic" 
-  vpc_id      = aws_vpc.vpc.id
+# resource "aws_security_group" "nagios_client" { 
+#   name        = "nagios_client" 
+#   description = "Allow db inbound traffic" 
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress { 
-    from_port   = 5693
-    to_port     = 5693
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]   
-} 
-  egress { 
-    from_port       = 0 
-    to_port         = 0 
-    protocol        = "-1" 
-    cidr_blocks     = ["0.0.0.0/0"] 
-  } 
-}
+#   ingress { 
+#     from_port   = 5693
+#     to_port     = 5693
+#     protocol    = "tcp" 
+#     cidr_blocks = ["0.0.0.0/0"]   
+# } 
+#   egress { 
+#     from_port       = 0 
+#     to_port         = 0 
+#     protocol        = "-1" 
+#     cidr_blocks     = ["0.0.0.0/0"] 
+#   } 
+# }
