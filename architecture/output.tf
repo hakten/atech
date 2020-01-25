870b701b22b5b0c08355e029ac6a6371ffad2c38
Huseyin Akten <husakten@gmail.com>
2020-01-24 22:37:58 -0600

    aesdf

--- architecture/output.tf
+++ architecture/output.tf
@@ -18,11 +18,11 @@ output "data_ami" {
   value = data.aws_ami.centos
 }
 
-output "aws_instance_nagios_public_ip" {
+output "nagios_public_ip" {
   value = aws_instance.nagios.public_ip
 }
 
-output "aws_instance_ansible_public_ip" {
+output "ansible_public_ip" {
   value = aws_instance.ansible.public_ip
 }
 
--- security-groups.tf
+++ security-groups.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "ssh" { 
-  name        = "ssh" 
-  description = "Allow SSH inbound traffic" 
+resource "aws_security_group" "ssh_bastion" { 
+  name        = "ssh_bastion" 
+  description = "Allow SSH inbound from bastion" 
   vpc_id      = module.vpc.vpc_id
 
   ingress { 
@@ -8,6 +8,26 @@ resource "aws_security_group" "ssh" {
     to_port     = 22 
     protocol    = "tcp" 
     cidr_blocks = ["${data.terraform_remote_state.bastion_public_ip}/32"]  
+
+} 
+  egress { 
+    from_port       = 0 
+    to_port         = 0 
+    protocol        = "-1" 
+    cidr_blocks     = ["0.0.0.0/0"] 
+  } 
+} 
+
+resource "aws_security_group" "ssh_ansible" { 
+  name        = "ssh_ansible" 
+  description = "Allow SSH inbound from ansible" 
+  vpc_id      = module.vpc.vpc_id
+
+  ingress { 
+    from_port   = 22 
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["${data.terraform_remote_state.ansible_public_ip}/32"]  
 
 } 
   egress { 
@@ -52,13 +72,13 @@ resource "aws_security_group" "nagios_client" {
     from_port   = 5693
     to_port     = 5693
     protocol    = "tcp" 
-    cidr_blocks = ["${data.aws_instance.nagios.public_ip}/32"]
+    cidr_blocks = ["${data.terraform_remote_state.ansible_public_ip}/32"]
 } 
   ingress { 
     from_port   = 0
     to_port     = 0
     protocol    = "icmp" 
-    cidr_blocks = ["${data.aws_instance.nagios.public_ip}/32"]
+    cidr_blocks = ["${data.terraform_remote_state.ansible_public_ip}/32"]
 }
   egress { 
     from_port       = 0 
