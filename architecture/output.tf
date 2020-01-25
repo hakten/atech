0ceeb2fdb67f6eab867953bd993636fb907e8a3e
Huseyin Akten <husakten@gmail.com>
2020-01-24 21:46:05 -0600

    env added

--- architecture/output.tf
+++ architecture/output.tf
@@ -14,6 +14,10 @@ output "private_subnets" {
   value = "${module.vpc.private_subnets}"
 }
 
+output "data_ami" {
+  value = data.aws_ami.centos
+}
+
 output "aws_instance_nagios_public_ip" {
   value = aws_instance.nagios.public_ip
 }
